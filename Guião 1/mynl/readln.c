63738e5fde10fc5db1e1904e982301561b435906
RisingFisan <sofiarsantos31@gmail.com>
2020-02-22 14:05:48 +0000

    Optimized readln functions

--- Guião 1/mynl/readln.c
+++ Guião 1/mynl/readln.c
@@ -6,26 +6,24 @@
 #include <stdio.h>
 
 ssize_t readln(int fd, char* line, size_t size) {
-    char* buffer = malloc(sizeof(char));
     ssize_t i = 0;
     while(i < size - 1) {
-        ssize_t bytes_read = read(fd, buffer, 1);
-        if(!bytes_read) break;
-        line[i++] = *buffer;
-        if(*buffer == '\n')
-            break;
+        ssize_t bytes_read = read(fd, &line[i], 1);
+        if(bytes_read < 1) break;
+        if(line[i++] == '\n') break;
     }
     line[i] = 0;
     return i;
 }
 
 ssize_t readln2(int fd, char* line, size_t size) {
-    size_t buffer_size = 1024;
-    char* buffer = malloc(buffer_size * sizeof(char));
-    ssize_t bytes_read = read(fd, buffer, buffer_size - 1);
-    size_t line_length = strcspn(buffer, "\n") + 1;
-    buffer[(line_length > size ? size : line_length)] = 0;
-    strcpy(line, buffer);
+    ssize_t bytes_read = read(fd, line, size);
+    if(!bytes_read) return 0;
+
+    size_t line_length = strcspn(line, "\n") + 1;
+    if(bytes_read < line_length) line_length = bytes_read;
+    line[line_length] = 0;
+    
     lseek(fd, line_length - bytes_read, SEEK_CUR);
-    return strlen(line);
+    return line_length;
 }
